728a2639a8750d7a6557f8198e2a0f122bfe4ba6
Jeremy Vidaurri <j.x.vidaurri@gmail.com>
2024-12-03 20:44:04 -0600

    Make day 3 part 2 more efficient

--- 2024/3/solution.py
+++ 2024/3/solution.py
@@ -22,35 +22,24 @@ def solve_part_one(input):
     return sum
 
 def solve_part_two(input):
-    queue_do = []
-    queue_dont = []
-    queue_mult = []
+    queue = []
     sum = 0
     single_string = ""
 
     for line in input:
         single_string += line.strip()
     
-    pattern_do = "do\(\)"
-    pattern_dont = "don\'t\(\)"
-    pattern_mult = "mul\(\d+,\d+\)"
+    pattern = r"(do\(\))|(don\'t\(\))|(mul\(\d+,\d+\))"
 
-    queue_do.extend(x.span() for x in re.finditer(pattern_do, single_string))
-    queue_dont.extend(x.span() for x in re.finditer(pattern_dont, single_string))
-    queue_mult.extend([x.span(), x.group()] for x in re.finditer(pattern_mult, single_string))
+    queue.extend(x.group() for x in re.finditer(pattern, single_string))
+
     currentMode = 'do'
-
-    for (start, end), string in queue_mult:
-        while (queue_do and start > queue_do[0][0]) or (queue_dont and start > queue_dont[0][0]):
-            if queue_do and start > queue_do[0][0]:
-                queue_do.pop(0)
-                currentMode = 'do'
-            if queue_dont and start > queue_dont[0][0]:
-                queue_dont.pop(0)
-                currentMode = 'dont'
-        if currentMode == 'do':
-            #print("MULT")
-            #print(start,end)
+    for string in queue:
+        if "don't()" in string:
+            currentMode = "dont"
+        elif "do()" in string:
+            currentMode = "do"
+        elif currentMode == "do":
             sum += multiply(string)
 
     return sum
