ea06e0d4bb801ab7392e8c73136a6985292e2b7a
Jeremy Vidaurri <j.x.vidaurri@gmail.com>
2024-12-07 20:53:07 -0600

    Day 7

--- 2024/7/solution.py
+++ 2024/7/solution.py
@@ -1,7 +1,45 @@
 # Advent of Code 2024 - Day 7
+import copy
+
+def brute_force(target, arr, cur_total):
+    if len(arr) == 0:
+        return target == cur_total
+
+    num = int(arr.pop(0))
+    add = brute_force(target, copy.deepcopy(arr), cur_total+num)
+    mul = brute_force(target, copy.deepcopy(arr), cur_total*num)
+    
+    return add or mul
+
+def brute_force_concat(target, arr, cur_total):
+    if len(arr) == 0:
+        return target == cur_total
+
+    num = int(arr.pop(0))
+    add = brute_force_concat(target, copy.deepcopy(arr), cur_total+num)
+    mul = brute_force_concat(target, copy.deepcopy(arr), cur_total*num)
+    combined = int(str(cur_total) + str(num))
+    concat = brute_force_concat(target, copy.deepcopy(arr), combined)
+    return add or mul or concat
 
 def solve_part_one(input):
-    return 0
+    lines = [line for line in input]
+    count = 0
+    for line in lines:
+        split = line.split()
+        split[0] = split[0][:-1] # Remove the ':' at the end
+        target = int(split.pop(0))
+        if brute_force(target, split, 0):
+            count += target
+    return count
 
 def solve_part_two(input):
-    return 0
+    lines = [line for line in input]
+    count = 0
+    for line in lines:
+        split = line.split()
+        split[0] = split[0][:-1] # Remove the ':' at the end
+        target = int(split.pop(0))
+        if brute_force_concat(target, split, 0):
+            count += target
+    return count
