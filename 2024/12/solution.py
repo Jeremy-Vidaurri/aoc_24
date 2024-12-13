c76234913b595fd7164f316c6564ecb93cf4cef4
Jeremy Vidaurri <j.x.vidaurri@gmail.com>
2024-12-12 18:19:56 -0600

    remove unnecessary comments

--- 2024/12/solution.py
+++ 2024/12/solution.py
@@ -36,13 +36,10 @@ def solve_part_one(input):
 
     return total
 
-# FOR LATER:
 def check_diags(grid, row, col, target):
     if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] != target:
         return True
     return False
-# diags are fucking up the seen 
-# set up a separate function that checks diags without the seen set
 
 def dfs2(grid, seen, values, row, col, target):
     if row < 0 or row >= len(grid) or col < 0 or col >= len(grid[0]) or grid[row][col] != target:
@@ -71,52 +68,43 @@ def dfs2(grid, seen, values, row, col, target):
     four_corners = set([0b1111])
     
 # you can only have an inner corner if you have 2 adjacent sides or 1 side or no side
-    one_edge = set([(0b1000, 0b0001), (0b1000, 0b0010), # Left is edge, top/bottom right does not match
-                    (0b0100, 0b1000), (0b0100, 0b0010), # Top is edge, top left/right is matching 
-                    (0b0010, 0b0100), (0b0010, 0b1000), # Right is edge, top/bottom right  is matching 
-                    (0b0001, 0b0100), (0b0001, 0b0001)]) # Bottom is edge, down rght/left is matching
+    one_edge = set([(0b1000, 0b0001), (0b1000, 0b0010),
+                    (0b0100, 0b1000), (0b0100, 0b0010),
+                    (0b0010, 0b0100), (0b0010, 0b1000),
+                    (0b0001, 0b0100), (0b0001, 0b0001)])
     
-    one_edge_double = set([(0b1000, 0b0011), # Left is edge, top/bottom right does not match
-                            (0b0100, 0b1010), # Top is edge, top left/right is matching 
-                            (0b0010, 0b1100), # Right is edge, top/bottom right  is matching 
-                            (0b0001, 0b0101),]) # Bottom is edge, down rght/left is matching
+    one_edge_double = set([(0b1000, 0b0011),
+                            (0b0100, 0b1010),
+                            (0b0010, 0b1100),
+                            (0b0001, 0b0101),])
     two_edge = set([(0b1100, 0b0010), (0b0110, 0b1000), (0b0011, 0b0100), (0b1001, 0b0001)])
 
-    #print(target, bin(valSides), row, col)
     if valSides in one_corner:
-        #print("ONE CORNER", row, col)
         values[1] += 1
     elif valSides in two_corners:
-        #print("TWO CORNER", row, col)
         values[1] += 2
     elif valSides in four_corners:
-        #print("FOUR CORNER", row, col)
         values[1] += 4
 
     flag = False
 
     for s, d in one_edge_double:
         if s == valSides and d & valDiag == d:
-           # print("ONE EDGE DOUBLE", row, col)
             values[1] += 2
             return False
 
     for s, d in one_edge:
         if s == valSides and d & valDiag == d:
-            #print("ONE EDGE", row, col)
             values[1] += 1
             return False
         
     for s, d in two_edge:
         if s == valSides and d & valDiag == d:
-            #print("TWO EDGE", row, col)
             values[1] += 1
             return False
         
     if valSides == 0 and valDiag != 0:
-        #print("NO EDGE", row, col)
         while valDiag:
-
             values[1] += valDiag & 1
             valDiag >>= 1
         return False
@@ -140,6 +128,5 @@ def solve_part_two(input):
             
             dfs2(grid, seen_sets[char], values, row, col, char)
             total += values[0] * values[1]
-            #print(char, values[0], values[1])
 
     return total
