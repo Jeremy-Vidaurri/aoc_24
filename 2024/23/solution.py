b1cf578728c1dddafff01051a7350fa27896414a
Jeremy-Vidaurri <j.x.vidaurri@gmail.com>
2024-12-27 17:58:08 -0600

    Day 23

--- 2024/23/solution.py
+++ 2024/23/solution.py
@@ -16,14 +16,42 @@ def solve_part_one(input):
     graph_adj = make_graph(connections)
     result = 0
     t_conns = set(['ta','tb','tc','td','te','tf','tg','th','ti','tj','tk','tl','tm','tn','to','tp','tq','tr','ts','tt','tu','tv','tw','tx','ty','tz',])
+    seen = set()
+    
     for c, conns in graph_adj.items():
-        
-        if len(conns) < 2 or (len(t_conns.union(conns)) == 0 and c not in t_conns):
+        if len(conns) < 2 or c not in t_conns:
             continue
-        print(c, conns)
-        result += 1
-
+        for c2 in conns:
+            for c3 in conns:
+                if c2 == c3 or c2 not in graph_adj[c3] or (c, c2, c3) in seen:
+                    continue
+                result += 1
+                seen.add((c, c2, c3))
+                seen.add((c, c3, c2))
+                seen.add((c2, c, c3))
+                seen.add((c2, c3, c))
+                seen.add((c3, c, c2))
+                seen.add((c3, c2, c))
     return result
 
+def bron_kerbosch(R, P, X, graph):
+    if not P and not X:
+        yield R
+    while P:
+        v = P.pop()
+        yield from bron_kerbosch(
+            R.union({v}),
+            P.intersection(graph[v]),
+            X.intersection(graph[v]),
+            graph
+        )
+        X.add(v)
+
 def solve_part_two(input):
-    return None
+    connections = [line.strip().split('-') for line in input if line != ""]
+    graph_adj = make_graph(connections)
+    all_cliques = list(bron_kerbosch(set(), set(graph_adj.keys()), set(), graph_adj))
+    max_clique_size = max(len(clique) for clique in all_cliques)
+    result = ''.join([','.join(sorted(clique)) for clique in all_cliques if len(clique) == max_clique_size])
+
+    return result
